aa85cb1036c2b729fd4d0d98626448496aff0b49
jonhenrikaavitsland <127969054+jonhenrikaavitsland@users.noreply.github.com>
2025-09-16 23:34:15 +0200

    astro add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,15 @@ import react from '@astrojs/react';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
   redirects: {
     "/posts": "/blog"
   }
